47a4b0694de9eb6e6a53f5652a3454c9b7ff9e00
David Quartarolo <david.quartarolo@gmail.com>
2022-12-23 12:53:31 -0500

    feat(setup): Added setup.py file for local install.

---
+++ setup.py
@@ -0,0 +1,32 @@
+"""
+DNS Recon setup.py
+"""
+
+from setuptools import setup
+
+setup(
+    name='dns_recon',
+    version='0.0.1',
+    description='DNS reconnaissance tool.',
+    packages=["dns_recon"],
+    py_modules=["dns_recon"],
+    package_dir={'dns_recon': 'dns_recon'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: System :: Networking :: Monitoring",
+        "Natural Language :: English"
+    ],
+    install_requires=[
+        "dnspython ~= 2.2.1"
+    ],
+    extras_require = {
+        "dev": [
+            "pylint>=2.15.0",
+        ],
+    },
+    url="https://github.com/but-i-am-dominator/dns_recon",
+    author="Dave Quartarolo",
+    author_email="david.quartarolo@gmail.com",
+)
